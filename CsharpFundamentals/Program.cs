b3a39fe3f5bf5e8473d523abfc0a6c863f507b71
Jackie <JGtheophila19@gmail.com>
2020-01-08 11:34:53 -0500

    creating an array of integers

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -10,8 +10,15 @@ namespace HelloWorld
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
+        {		
+			// creating an array of integers, {initialize objects}
+			// int[] numbers = new int[] {3, 7, 9, 2, 14, 6};
+            
+			// can make the line aove shorter
+			var number = new[] {3, 7, 9, 2, 14, 6};
+			
+			// Length property - size of the array
+			Console.WriteLine("Length: " + numbers.Length); // 6
         }
     }
 }
