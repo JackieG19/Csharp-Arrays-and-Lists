df7b414cc23f809b99a0e6fa54dc67769fb27009
Jackie <JGtheophila19@gmail.com>
2020-01-08 12:38:41 -0500

    using the Clear method with the array

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -13,11 +13,14 @@ namespace HelloWorld
         {		
 		var number = new[] {3, 7, 9, 2, 14, 6};
 		
-		// IndexOf() method - to find the position of an element
-		var index = Array.IndexOf(numbers, 9); 
-		
-		// (first parameter = array, second parameter = the object)
-		Console.WriteLine("Index of 9: " + index); // 2
+		// Clear() method
+		Array.Clear(numbers, 0, 2);
+		// (array, starting index of the range of elements)
+			
+		Console.WriteLine("Effect of Clear()");
+			
+		foreach (var n in number)
+			Console.WriteLine(n);
         }
     }
 }
