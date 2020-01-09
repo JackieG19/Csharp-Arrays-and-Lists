65ecfb0aec2c4262cd6bd4f18d07dc345c6d976f
Jackie <JGtheophila19@gmail.com>
2020-01-09 10:39:04 -0500

    adding the 1 object to the list

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -12,7 +12,11 @@ namespace HelloWorld
         static void Main(string[] args)
         {		
 		// creating a list of integers(initialize/store objects)
-		var number = new List<int>() {1, 2, 3, 4};	
+		var number = new List<int>() {1, 2, 3, 4};
+		
+		// adding the 1 object to the list
+		numbers.Add(1); 
+
         }
     }
 }
